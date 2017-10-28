7319cfc8df3ab498232acf478c6ec00624239217
uehara <b1015062@fun.ac.jp>
2017-10-29 01:36:14 +0900

    shortcut

--- TutoRealServer/TutoReal/api/v1.py
+++ TutoRealServer/TutoReal/api/v1.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url, include
-from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
@@ -10,77 +9,17 @@ from rest_framework.permissions import AllowAny
 router = routers.DefaultRouter()
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-router.register(r'users', UserViewSet)
-
-
-class Beacon(models.Model):
-    major = models.PositiveIntegerField()
-    minor = models.PositiveIntegerField()
-
-    class Meta:
-        unique_together = ('major', 'minor')
-
-    def __str__(self):
-        return f'major: {self.major}\tminor: {self.minor}'
-
-
-class BeaconSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Beacon
-        fields = ('id', 'major', 'minor')
-
-
-class BeaconViewSet(viewsets.ModelViewSet):
-    permission_classes = (AllowAny, )
-    queryset = Beacon.objects.all()
-    serializer_class = BeaconSerializer
-
-router.register(r'beacons', BeaconViewSet)
-
-
 class Tutorial(models.Model):
     name = models.CharField('チュートリアルの名前', max_length=64)
+    major = models.PositiveIntegerField()
+    minor = models.PositiveIntegerField()
     image_path = models.ImageField('サムネイル')
-    beacon = models.OneToOneField(Beacon,  null=False, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
-
-class TutorialSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Tutorial
-        fields = ('id', 'name', 'image_path', 'beacon')
-
-
-class TutorialViewSet(viewsets.ModelViewSet):
-    permission_classes = (AllowAny, )
-    queryset = Tutorial.objects.all()
-    serializer_class = TutorialSerializer
-    filter_backends = (DjangoFilterBackend, )
-    filter_fields = ('beacon', )
-
-    def retrieve(self, request, *args, **kwargs):
-        major = request.GET['major']
-        minor = request.GET['minor']
-        try:
-            the_tutorial = Tutorial.objects.get(beacon=Beacon.objects.get(major=major, minor=minor))
-        except (Tutorial.DoesNotExist, Beacon.DoesNotExist):
-            raise Http404
-        return Response(TutorialSerializer(the_tutorial).data)
-
-router.register(r'tutorials', TutorialViewSet)
+        unique_together = ('major', 'minor')
 
 
 class Skill(models.Model):
@@ -89,22 +28,6 @@ class Skill(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class SkillSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Skill
-        fields = ('id', 'name', 'tutorial')
-
-
-class SkillViewSet(viewsets.ModelViewSet):
-    permission_classes = (AllowAny, )
-    queryset = Skill.objects.all()
-    serializer_class = SkillSerializer
-    filter_backends = (DjangoFilterBackend, )
-    filter_fields = ('tutorial', )
-
-router.register(r'skills', SkillViewSet)
 
 
 class Task(models.Model):
@@ -123,6 +46,22 @@ class TaskSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'skill', 'image_path', 'description')
 
 
+class SkillSerializer(serializers.ModelSerializer):
+    task_set = TaskSerializer(many=True)
+
+    class Meta:
+        model = Skill
+        fields = ('id', 'name', 'tutorial', 'task_set')
+
+
+class TutorialSerializer(serializers.ModelSerializer):
+    skill_set = SkillSerializer(many=True)
+
+    class Meta:
+        model = Tutorial
+        fields = ('id', 'major', 'minor', 'name', 'image_path', 'skill_set')
+
+
 class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = (AllowAny, )
     queryset = Task.objects.all()
@@ -132,6 +71,34 @@ class TaskViewSet(viewsets.ModelViewSet):
 
 router.register(r'tasks', TaskViewSet)
 
+
+class SkillViewSet(viewsets.ModelViewSet):
+    permission_classes = (AllowAny, )
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
+    filter_backends = (DjangoFilterBackend, )
+    filter_fields = ('tutorial', )
+
+router.register(r'skills', SkillViewSet)
+
+
+class TutorialViewSet(viewsets.ModelViewSet):
+    permission_classes = (AllowAny, )
+    queryset = Tutorial.objects.all()
+    serializer_class = TutorialSerializer
+    filter_backends = (DjangoFilterBackend, )
+
+    def list(self, request, *args, **kwargs):
+        major = request.GET['major']
+        minor = request.GET['minor']
+        try:
+            the_tutorial = Tutorial.objects.get(major=major, minor=minor)
+        except Tutorial.DoesNotExist:
+            raise Http404
+        return Response(TutorialSerializer(the_tutorial).data)
+
+
+router.register(r'tutorials', TutorialViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
