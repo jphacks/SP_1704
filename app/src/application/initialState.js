2c17bd4b3b1a59d48da95f16a720ca08f346a52a
i544c <leo@i544c.me>
2017-10-28 15:29:04 +0900

    [add]success to receave ibeacon

--- app/src/application/initialState.js
+++
@@ -1,5 +0,0 @@
-const initial_state = {
-  count: 0,
-};
-
-export default initial_state;
--- app/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, combineReducers } from 'redux'
-import application from '../application/reducer';
-
-const store = createStore(combineReducers({
-  application,
-}));
-
-export default store;
