5938a9002c33dc3d87f04c3f5e86a9a860af1698
i544c <leo@i544c.me>
2017-10-29 03:48:57 +0900

    [modify] show tutoreals

--- app/src/ui/TutorealList/index.js
+++ app/src/ui/TutorealList/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import boundActionCreator from '../boundActionCreator';
+import { StyleSheet, Text, View, TouchableOpacity, Image, ScrollView } from 'react-native';
+import boundActiionCreator from '../boundActionCreator';
 import * as types from '../../application/types';
 
 import TaskView from '../TaskView/index';
@@ -9,16 +9,77 @@ import TutorealView from '../TutorealView/index';
 
 
 class TutorealList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tutorials: [
+        {
+          title: "そば",
+          img: "https://lohas.nicoseiga.jp//thumb/6494740cz?",
+        },
+        {
+          title: "ピザ",
+          img: "https://lohas.nicoseiga.jp/thumb/6746922i?1495815505",
+        },
+        {
+          title: "あ",
+          img: "https://lohas.nicoseiga.jp/thumb/6746922i?1495815505",
+        },
+        {
+          title: "い",
+          img: "https://lohas.nicoseiga.jp/thumb/6746922i?1495815505",
+        },
+        {
+          title: "う",
+          img: "https://lohas.nicoseiga.jp/thumb/6746922i?1495815505",
+        },
+        {
+          title: "え",
+          img: "https://lohas.nicoseiga.jp/thumb/6746922i?1495815505",
+        },
+        {
+          title: "お",
+          img: "https://lohas.nicoseiga.jp/thumb/6746922i?1495815505",
+        },
+        {
+          title: "か",
+          img: "https://lohas.nicoseiga.jp/thumb/6746922i?1495815505",
+        },
+        {
+          title: "き",
+          img: "https://lohas.nicoseiga.jp/thumb/6746922i?1495815505",
+        },
+      ],
+    };
+  }
+
   render() {
     const state = this.props.state;
     return (
-      <View>
-        <Text>tutoreal list</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.tutorealList}>
+          {this.state.tutorials.map((tutoreal) => {
+            return (
+              <View key={tutoreal.title} style={styles.card}>
+                <Text>{tutoreal.title}</Text>
+                <Image
+                  style={{width: 100, height: 100, resizeMode: 'cover'}}
+                  source={{uri: tutoreal.img}}
+                />
+              </View>
+            );
+          })}
+        </View>
+      </ScrollView>
     );
   }
 }
 export default connect(state => ({state}))(TutorealList);
 
 const styles = StyleSheet.create({
+  tutorealList: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
 });
