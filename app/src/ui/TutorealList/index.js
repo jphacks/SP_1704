efc4abe4bfc9fcd9d2ae94f2f68be4310a6c1099
i544c <leo@i544c.me>
2017-10-29 04:27:18 +0900

    [add] modify tutorealList

--- app/src/ui/TutorealList/index.js
+++ app/src/ui/TutorealList/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { StyleSheet, Text, View, TouchableOpacity, Image, ScrollView } from 'react-native';
-import boundActiionCreator from '../boundActionCreator';
+import boundActionCreator from '../boundActionCreator';
 import * as types from '../../application/types';
 
 import TaskView from '../TaskView/index';
@@ -12,61 +12,24 @@ class TutorealList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tutorials: [
-        {
-          title: "そば",
-          img: "https://lohas.nicoseiga.jp//thumb/6494740cz?",
-        },
-        {
-          title: "ピザ",
-          img: "https://lohas.nicoseiga.jp/thumb/6746922i?1495815505",
-        },
-        {
-          title: "あ",
-          img: "https://lohas.nicoseiga.jp/thumb/6746922i?1495815505",
-        },
-        {
-          title: "い",
-          img: "https://lohas.nicoseiga.jp/thumb/6746922i?1495815505",
-        },
-        {
-          title: "う",
-          img: "https://lohas.nicoseiga.jp/thumb/6746922i?1495815505",
-        },
-        {
-          title: "え",
-          img: "https://lohas.nicoseiga.jp/thumb/6746922i?1495815505",
-        },
-        {
-          title: "お",
-          img: "https://lohas.nicoseiga.jp/thumb/6746922i?1495815505",
-        },
-        {
-          title: "か",
-          img: "https://lohas.nicoseiga.jp/thumb/6746922i?1495815505",
-        },
-        {
-          title: "き",
-          img: "https://lohas.nicoseiga.jp/thumb/6746922i?1495815505",
-        },
-      ],
+      tutorials: [...Array.from({length: 200}, (x, i) => i).map(x => ({title: x, img: "https://lohas.nicoseiga.jp//thumb/6494740cz?"}))],
     };
   }
 
   render() {
     const state = this.props.state;
     return (
-      <ScrollView>
+      <ScrollView style={styles.tutorealListContainer}>
         <View style={styles.tutorealList}>
-          {this.state.tutorials.map((tutoreal) => {
+          {this.props.state.application.tutoreals.map((tutoreal) => {
             return (
-              <View key={tutoreal.title} style={styles.card}>
-                <Text>{tutoreal.title}</Text>
+              <TouchableOpacity key={tutoreal.name} style={styles.card} onPress={() => boundActionCreator(types.SET_VIEW_STATE, {view_state: "tutoreal_view"})}>
+                <Text>{tutoreal.name}</Text>
                 <Image
                   style={{width: 100, height: 100, resizeMode: 'cover'}}
-                  source={{uri: tutoreal.img}}
+                  source={{uri: tutoreal.image_path}}
                 />
-              </View>
+              </TouchableOpacity>
             );
           })}
         </View>
@@ -77,9 +40,18 @@ class TutorealList extends React.Component {
 export default connect(state => ({state}))(TutorealList);
 
 const styles = StyleSheet.create({
+  tutorealListContainer: {
+    flex: 1,
+  },
   tutorealList: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
     flexDirection: 'row',
     flexWrap: 'wrap',
+  },
+  card: {
+    width: 100,
+    height: 100,
   },
 });
